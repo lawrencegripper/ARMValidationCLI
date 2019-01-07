75820f6123d0a741f0cc866359b9a5d9f7e384b2
Lawrence Gripper <info@grippers.co.uk>
2019-01-07 15:09:11 +0000

    Added schema validation

--- index.ts
+++ index.ts
@@ -1,28 +1,51 @@
 import { DeploymentTemplate } from "./vscode-azurearmtools/src/DeploymentTemplate"
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import * as http from "http";
-import * as ajvValidator from "ajv";
-import * as request from "request-promise"
+import { getLanguageService, JSONDocument } from "vscode-json-languageservice"
+import { TextDocument } from 'vscode-languageserver-types';
+import { xhr, XHRResponse, configure as configureHttpRequests, getErrorStatusDescription } from 'request-light';
 
-
+// Added to prevent AppInsights code in vscode-azurearmtools creating build errors
 declare module "http" {
     export class ServerRequest extends http.IncomingMessage { }
 }
 
-async function getErrors() {
+// Define how json schemas should be fetched
+const schemaRequestService = (uri: string): Thenable<string> => {
+	
+	const headers = { 'Accept-Encoding': 'gzip, deflate' };
+	return xhr({ url: uri, followRedirects: 5, headers }).then(response => {
+		return response.responseText;
+	}, (error: XHRResponse) => {
+		return Promise.reject(error.responseText || getErrorStatusDescription(error.status) || error.toString());
+	});
+};
 
+async function getErrors() {
     var content = readFileSync("./testdata/basic.1error.json")
 
     var template = new DeploymentTemplate(content.toString(), "./testdata/basic.json")
 
-    // Without this call schema errors aren't show
     if (!template.hasValidSchemaUri()) {
         console.log("invalid schema uri")
         throw "can't continue without valid schema"
     } else {
-        //Todo Schema validation here...
+        //Use the VSCode JSON language server to validate the schema
+
+        let service = getLanguageService({ schemaRequestService: schemaRequestService })
+
+        let document = toDocument(content.toString())
+        let jsonDoc = await service.parseJSONDocument(document)
+
+        let results = await service.doValidation(document, jsonDoc)
+
+        results.forEach(e =>{
+            console.log(e)
+        })
+
     }
 
+    // Use the VSCode ARM extension to validate the ARM template syntax
     var errors = await template.errors
 
     errors.forEach(error => {
@@ -40,7 +63,7 @@ async function getErrors() {
 
 getErrors().then(() => console.log("done")).catch(x => console.log(x))
 
-// async function addSchema(schemaUri: string, schema: ajvValidator.Ajv) {
-//     var r = await request(schemaUri);
-//     schema.addSchema(JSON.parse(r));
-// }
+function toDocument(text: string): TextDocument {
+	return TextDocument.create('foo://bar/file.json', 'json', 0, text);
+}
+
