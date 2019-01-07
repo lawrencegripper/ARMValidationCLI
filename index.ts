6611cf69859f1c9074fbc40572c31054678a7ba9
Lawrence Gripper <info@grippers.co.uk>
2019-01-07 12:06:04 +0000

    Remove faliing schmea validation logic

--- index.ts
+++ index.ts
@@ -20,35 +20,7 @@ async function getErrors() {
         console.log("invalid schema uri")
         throw "can't continue without valid schema"
     } else {
-        var ajv = new ajvValidator({
-            meta: false, // optional, to prevent adding draft-06 meta-schema
-            extendRefs: true, // optional, current default is to 'fail', spec behaviour is to 'ignore'
-            unknownFormats: 'ignore',  // optional, current default is true (fail)
-        })
-
-        var metaSchema = require('ajv/lib/refs/json-schema-draft-04.json');
-        ajv.addMetaSchema(metaSchema);
-        ajv._opts.defaultMeta = metaSchema.id;
-        
-        // optional, using unversioned URI is out of spec, see https://github.com/json-schema-org/json-schema-spec/issues/216
-        ajv._refs['http://json-schema.org/schema'] = 'http://json-schema.org/draft-04/schema';
-        
-        // Optionally you can also disable keywords defined in draft-06
-        ajv.removeKeyword('propertyNames');
-        ajv.removeKeyword('contains');
-        ajv.removeKeyword('const');
-
-        var schema = ajv
-
-        // await addSchema("http://json-schema.org/draft-04/schema#", schema);
-        await addSchema(template.schemaUri, schema);
-        var valid = schema.validate(template.schemaUri, content)
-        if (!valid) {
-            schema.errors.forEach(error => {
-                console.log(error)
-            })
-        }
-
+        //Todo Schema validation here...
     }
 
     var errors = await template.errors
@@ -68,7 +40,7 @@ async function getErrors() {
 
 getErrors().then(() => console.log("done")).catch(x => console.log(x))
 
-async function addSchema(schemaUri: string, schema: ajvValidator.Ajv) {
-    var r = await request(schemaUri);
-    schema.addSchema(JSON.parse(r));
-}
+// async function addSchema(schemaUri: string, schema: ajvValidator.Ajv) {
+//     var r = await request(schemaUri);
+//     schema.addSchema(JSON.parse(r));
+// }
