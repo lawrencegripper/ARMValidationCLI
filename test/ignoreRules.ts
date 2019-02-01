cd5e7320ac0c024e98357f26dc03f72a13d95914
Lawrence Gripper <info@grippers.co.uk>
2019-02-01 16:31:36 +0000

    Improve test coverage for `resource` ignores. Update ignore syntax in readme

--- test/ignoreRules.ts
+++ test/ignoreRules.ts
@@ -30,50 +30,6 @@ describe('Ignore rules test', () => {
         expect(s.length).to.equal(0);
     }).timeout(timeoutValue);
 
-    it('error if both jsonPath and resource set in an ignore rule', async () => {
-        let ignoreRules = JSON.parse(`{
-            "test/testdata/azuredeploy.arm.1error.json": [
-                {
-                    "message": "Unrecognized function name 'nonfunction'.",
-                    "jsonPath": "resources.1.dependsOn.1",
-                    "resource": {
-                        "name": "bob"
-                    }
-                }
-            ]
-        }`);
-
-        // tslint:disable-next-line:no-unused-expression
-        try {
-            let x = await getErrorsForFile("test/testdata/azuredeploy.arm.1error.json", ignoreRules);
-        } catch (e) {
-            expect(e.message).to.equal(new Error("Cannot specify both 'jsonPath' and 'resource' in an ignore rule").message);
-            return;
-        }
-
-        fail("Expected error");
-    }).timeout(timeoutValue);
-
-    it('error if neither jsonPath and resource set in an ignore rule', async () => {
-        let ignoreRules = JSON.parse(`{
-            "test/testdata/azuredeploy.arm.1error.json": [
-                {
-                    "message": "Unrecognized function name 'nonfunction'."
-                }
-            ]
-        }`);
-
-        // tslint:disable-next-line:no-unused-expression
-        try {
-            let x = await getErrorsForFile("test/testdata/azuredeploy.arm.1error.json", ignoreRules);
-        } catch (e) {
-            expect(e.message).to.equal(new Error("Must specify either 'jsonPath' and 'resource' in an ignore rule").message);
-            return;
-        }
-
-        fail("Expected error");
-    }).timeout(timeoutValue);
-
     it('when global ignore configured it skips error', async () => {
         let ignoreRules = JSON.parse(`{
             "global": [
